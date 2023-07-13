fa507edffe2dd4e43d718d47a5eeda519aa8e777
jaythree <giljr.2009@gmail.com>
2023-07-13 18:44:20 -0400

    :sparkles: feat: add views to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_221836) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_193131) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
