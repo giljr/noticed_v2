66024abb642c42d104bc5deb67637f77c3210be2
Gilberto Oliveira Jr <giljr.2009@gmail.com>
2024-03-31 10:08:53 -0400

    :ok_hand: :review: app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  # By implementing this feature, users will be able to conveniently
+  # associate and access all notifications directed towards them.
   has_many :notifications, as: :recipient, dependent: :destroy, class_name: 'Noticed::Notification'
-  has_many :notification_mentions, through: :user, dependent: :destroy
+  # Whenever you have noticed events that have the record pointing to the user,
+  # such as when a new user joins a team or any similar occurrences,
+  # It's important to ensure that notifications mentioning us are accessible.
+  has_many :notification_mentions, as: :record, dependent: :destroy, class_name: 'Noticed::Event'
 end
